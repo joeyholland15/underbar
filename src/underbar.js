2ac25d2de8b8c65e582287123c8accad14bbf979
Joey Holland <joeyholland15@gmail.com>
2015-10-06 14:29:31 -0700

    Completed reject function tests

--- src/underbar.js
+++ src/underbar.js
@@ -95,6 +95,10 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it;
+    var results = _.filter(collection, function(element) {
+    	return test(element) === false;
+    });
+    return results;
   };
 
   // Produce a duplicate-free version of the array.
